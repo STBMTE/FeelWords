f9170bd912fa13bb5312882ad30f7368ca45221a
Андрей <ateruhin388@gmail.com>
2020-11-13 19:42:05 +0400

    start

---
+++ ConsoleApp1/Menu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FeelWords
+{
+	class consolewrite
+	{
+		public void NewGame()
+		{
+			Console.WriteLine("NEW GAME");
+		}
+		public void Continue()
+		{
+			Console.WriteLine("CONTINUE");
+		}
+		public void Rating()
+		{
+			Console.WriteLine("RATING");
+		}//рейтинг
+		public void Exit()
+        {
+			Console.WriteLine("EXIT");
+        }
+	}
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FeelWords
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
